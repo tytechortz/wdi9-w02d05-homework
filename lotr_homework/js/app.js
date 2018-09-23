e99e6eae35a978be0bc55a4358fa1d0c7e73be7a
James <tytechortz@gmail.com>
2018-09-22 22:52:32 -0600

    Chapter 1 complete - Made Middle Earth

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -48,8 +48,11 @@ const makeMiddleEarth = () => {
 
   // 1. create a section tag with an id of middle-earth
 
-  // 2. append the section to the body of the DOM.
+const $body = $('body');
+const $section = $('<section id="middle-earth">Middle Earth');
 
+  // 2. append the section to the body of the DOM.
+$body.append($section);
   // 3. use a for loop to iterate over the lands array that does the following:
 
   //   3a. creates an article tag (there should be one for each land when the loop is done)
@@ -59,8 +62,19 @@ const makeMiddleEarth = () => {
   //   3c. includes an h1 with the name of the land inside each land article
 
   //   3d. appends each land to the middle-earth section
+  
+  for(let i = 0; i < lands.length; i++) {
+    const $article = $("<article/>").attr("id",lands[i]);
+    $section.append($article);
+    const $h1 = $('<h1/>').text(lands[i]);
+    $article.append($h1);
+  }
+  // for(let i=0; i<lands.length; i++){
+  //     $('article id=' + lands[i] +'/>').text(<h1/>)
 
-};
+      // const $article = $('<article id='+(lands[i])+'<article/>');
+      // $section.append($article);
+}
 
 // COMMIT YOUR WORK
 // The commit message should read: "Chapter 1 complete - Made Middle Earth".
